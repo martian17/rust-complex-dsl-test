1fadd4d36c3de3d292b2a3808cf3a423a5d0900b
Yutaro Yoshii <yakumodanshi@gmail.com>
2025-12-17 16:36:46 +0900

    Split the dsl api in v1 and v2, and use num-complex instead of num::complex

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-use dsl_parser::{test_constant, complex};
+use dsl_parser::{poc_macro, complex_v1, complex_v2};
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use num::complex::Complex;
+    use num_complex::Complex;
 
     #[test]
     fn it_works() {
-        let result = test_constant!(2.4*5+3i);
+        let result = poc_macro!(2.4*5+3i);
         assert_eq!(result, Complex::new(2.3,3.4));
     }
     #[test]
+    fn it_works1() {
+        let result = complex_v1!(2.4*5+3i);
+        //let result = complex_v1!(2.2);
+        assert_eq!(result, Complex::new(12.0,3.0));
+    }
+    #[test]
     fn it_works2() {
-        let result = complex!(2.4*5+3i);
-        //let result = complex!(2.2);
+        let result = complex_v2!(2.4*5+3i);
+        //let result = complex_v1!(2.2);
         assert_eq!(result, Complex::new(12.0,3.0));
     }
 }
